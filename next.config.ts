a01263073bc8412f0b802581768c84dabcc2b37b
Abdul Moeez Shaikh <abdulmoeezshaikh@gmail.com>
2025-07-12 00:22:51 -0400

    proxy for rewrite

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,18 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "*" },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
