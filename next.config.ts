772df4f4bffa5541f5d5566dbebfe6176875dbb8
Abdul Moeez Shaikh <abdulmoeezshaikh@gmail.com>
2025-07-14 19:06:48 -0400

    Add iframe headers for proxy support

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,8 @@ const nextConfig: NextConfig = {
           { key: "Access-Control-Allow-Origin", value: "*" },
           { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
           { key: "Access-Control-Allow-Headers", value: "*" },
+          { key: "X-Frame-Options", value: "ALLOWALL" },
+          { key: "Content-Security-Policy", value: "frame-ancestors *;" },
         ],
       },
     ];
